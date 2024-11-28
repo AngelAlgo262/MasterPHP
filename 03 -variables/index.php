c1358731b3ae58f3a7482af7d96259feee9ff820
Angel Ocampo <angelocampo262@gmail.com>
2024-11-28 08:14:06 -0600

    Variables y tipos de datos

---
+++ 03 -variables/index.php
@@ -0,0 +1,16 @@
+<?php
+
+//Una variable es un contenedor que guarda cualquier informmacion
+$variable = "<h1> Hola mundo desde una variable </h1>";
+$numero = 45;
+$verdadero = true;
+$numero =777;
+echo $variable;
+echo $numero."<br>";
+
+$numero=120;
+echo $numero;
+
+
+
+?>
